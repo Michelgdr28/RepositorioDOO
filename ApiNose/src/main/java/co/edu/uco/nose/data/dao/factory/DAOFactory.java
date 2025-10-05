11bd3dd5a4d0a368682aff1b23820a987c774f9f
Michelgdr28 <michel.guarnizo@udea.edu.co>
2025-10-04 19:01:55 -0500

    Actualizacion DAOFactory
    Renombramiento clases

--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/factory/DAOFactory.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/factory/DAOFactory.java
@@ -4,45 +4,47 @@ import java.sql.Connection;
 
 import co.edu.uco.nose.data.dao.entity.CityDAO;
 import co.edu.uco.nose.data.dao.entity.CountryDAO;
-import co.edu.uco.nose.data.dao.entity.DepartmentDAO;
 import co.edu.uco.nose.data.dao.entity.IdentificationTypeDAO;
+import co.edu.uco.nose.data.dao.entity.DepartmentDAO;
 import co.edu.uco.nose.data.dao.entity.UserDAO;
 
-//CLASE ABSTRACTA= NO SE PUEDE INSTANCIAR
 public abstract class DAOFactory {
-	
+
 	protected Connection connection;
 	protected FactoryEnum factory = FactoryEnum.SQLSERVER;
-
+	
 	public static DAOFactory getFactory() {
 		return null;
 	}
-	
+
 	public abstract CityDAO getCityDAO();
-	
-	public abstract CountryDAO getcountryDAO();
-	
-	public abstract IdentificationTypeDAO getIdentificationTypeDAO();
-	
-	public abstract DepartmentDAO getDepartmentDAO();
-	
-	public abstract UserDAO getUserDAO();
-	
-	protected abstract void openConnection();
-	
+
+	public abstract CountryDAO getCountryDAO();
+
+	public abstract IdentificationTypeDAO getIdType();
+
+	public abstract DepartmentDAO getStateDAO();
+
+	public abstract UserDAO getuserUserDAO();
+
+	protected final void openConnection() {
+
+	}
+
 	protected final void initTransaction() {
-		
+
 	}
-	
+
 	protected final void commitTransaction() {
-		
+
 	}
-	
+
 	protected final void rollbackTransaction() {
-		
+
 	}
-	
+
 	protected final void closeConnection() {
-		
+
 	}
+
 }
