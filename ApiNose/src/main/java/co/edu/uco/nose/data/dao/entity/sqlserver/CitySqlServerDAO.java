2999d87a709bfe995854d82cd22452c7f7eef108
Michelgdr28 <michel.guarnizo@udea.edu.co>
2025-10-08 20:22:22 -0500

    sqlserverdao.2

--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/CitySqlServerDAO.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/CitySqlServerDAO.java
@@ -1,29 +1,142 @@
 package co.edu.uco.nose.data.dao.entity.sqlserver;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import co.edu.uco.nose.crosscuting.exception.NoseException;
+import co.edu.uco.nose.crosscuting.helper.TextHelper;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+import co.edu.uco.nose.crosscuting.messagescatalog.MessagesEnum;
 import co.edu.uco.nose.data.dao.entity.CityDAO;
+import co.edu.uco.nose.data.dao.entity.SqlConnection;
 import co.edu.uco.nose.entity.CityEntity;
 
-public final class CitySqlServerDAO implements CityDAO{
+public final class CitySqlServerDAO extends SqlConnection implements CityDAO {
 
-	@Override
-	public List<CityEntity> finAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public CitySqlServerDAO(final java.sql.Connection connection) {
+        super(connection);
+    }
 
-	@Override
-	public List<CityEntity> findByfilter(CityEntity filterEntity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<CityEntity> finAll() {
+        final String sql = "SELECT id, department_id, name FROM city";
+        final List<CityEntity> results = new ArrayList<>();
 
-	@Override
-	public CityEntity findById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        try (PreparedStatement ps = getConnection().prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
+            while (rs.next()) {
+                results.add(mapResultSetToCityEntity(rs));
+            }
+            return results;
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            throw NoseException.create(userMessage, technicalMessage);
+        }
+    }
+
+    @Override
+    public List<CityEntity> findByfilter(final CityEntity filterEntity) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("SELECT id, department_id, name FROM city WHERE 1=1");
+
+        final List<Object> params = new ArrayList<>();
+
+        if (filterEntity != null) {
+            final UUID defaultUUID = UUIDHelper.getUUIDHelper().getDefault();
+            if (filterEntity.getId() != null && !filterEntity.getId().equals(defaultUUID)) {
+                sb.append(" AND id = ?");
+                params.add(filterEntity.getId());
+            }
+
+            if (filterEntity.getDepartmentId() != null && !filterEntity.getDepartmentId().equals(defaultUUID)) {
+                sb.append(" AND department_id = ?");
+                params.add(filterEntity.getDepartment());
+            }
+
+            if (!TextHelper.isEmpty(filterEntity.getName())) {
+                sb.append(" AND name = ?");
+                params.add(filterEntity.getName());
+            }
+        }
+
+        final List<CityEntity> results = new ArrayList<>();
+        try (PreparedStatement ps = getConnection().prepareStatement(sb.toString())) {
+            for (int i = 0; i < params.size(); i++) {
+                ps.setObject(i + 1, params.get(i));
+            }
+
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    results.add(mapResultSetToCityEntity(rs));
+                }
+            }
+
+            return results;
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            throw NoseException.create(userMessage, technicalMessage);
+        }
+    }
+
+    @Override
+    public CityEntity findById(final UUID id) {
+        final String sql = "SELECT id, department_id, name FROM city WHERE id = ?";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setObject(1, id);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return mapResultSetToCityEntity(rs);
+                }
+            }
+
+            return null;
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_EXECUTING_STATEMENT.getContent()
+            throw NoseException.create(userMessage, technicalMessage);
+        }
+    }
+
+    private CityEntity mapResultSetToCityEntity(final ResultSet rs) throws SQLException {
+        final CityEntity entity = new CityEntity();
+
+        try {
+            final Object idObj = rs.getObject("id");
+            if (idObj instanceof UUID) {
+                entity.setId((UUID) idObj);
+            } else {
+                final String idStr = rs.getString("id");
+                entity.setId(idStr == null ? UUIDHelper.getUUIDHelper().getDefault() : UUID.fromString(idStr));
+            }
+        } catch (final Exception exception) {
+            entity.setId(UUIDHelper.getUUIDHelper().getDefault());
+        }
+
+        try {
+            final Object deptObj = rs.getObject("department_id");
+            if (deptObj instanceof UUID) {
+                entity.setDepartmentId((UUID) deptObj);
+            } else {
+                final String deptStr = rs.getString("department_id");
+                entity.setDepartmentId(deptStr == null ? UUIDHelper.getUUIDHelper().getDefault() : UUID.fromString(deptStr));
+            }
+        } catch (final Exception ex) {
+            entity.setDepartmentId(UUIDHelper.getUUIDHelper().getDefault());
+        }
+
+    
+        entity.setName(rs.getString("name"));
+
+        return entity;
+    }
 }
+
--- ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/DepartmentSqlServerDAO.java
+++ ApiNose/src/main/java/co/edu/uco/nose/data/dao/entity/sqlserver/DepartmentSqlServerDAO.java
@@ -1,29 +1,131 @@
 package co.edu.uco.nose.data.dao.entity.sqlserver;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import co.edu.uco.nose.crosscuting.exception.NoseException;
+import co.edu.uco.nose.crosscuting.helper.UUIDHelper;
+import co.edu.uco.nose.crosscuting.messagescatalog.MessagesEnum;
 import co.edu.uco.nose.data.dao.entity.DepartmentDAO;
+import co.edu.uco.nose.data.dao.entity.SqlConnection;
 import co.edu.uco.nose.entity.DepartmentEntity;
 
-public final class DepartmentSqlServerDAO implements DepartmentDAO{
+public final class DepartmentSqlServerDAO extends SqlConnection implements DepartmentDAO {
 
-	@Override
-	public List<DepartmentEntity> finAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public DepartmentSqlServerDAO(final java.sql.Connection connection) {
+        super(connection);
+    }
 
-	@Override
-	public List<DepartmentEntity> findByfilter(DepartmentEntity filterEntity) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<DepartmentEntity> finAll() {
+        final String sql = "SELECT id, country_id, name FROM department";
+        final List<DepartmentEntity> results = new ArrayList<>();
 
-	@Override
-	public DepartmentEntity findById(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        try (PreparedStatement ps = getConnection().prepareStatement(sql);
+             ResultSet rs = ps.executeQuery()) {
 
+            while (rs.next()) {
+                results.add(mapResultSetToDepartmentEntity(rs));
+            }
+            return results;
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            throw NoseException.create(userMessage, technicalMessage);
+        }
+    }
+
+    @Override
+    public List<DepartmentEntity> findByfilter(final DepartmentEntity filterEntity) {
+        final StringBuilder sb = new StringBuilder();
+        sb.append("SELECT id, country_id, name FROM department WHERE 1=1");
+        final List<Object> params = new ArrayList<>();
+        final UUID defaultUUID = UUIDHelper.getUUIDHelper().getDefault();
+
+        if (filterEntity != null) {
+            if (filterEntity.getId() != null && !filterEntity.getId().equals(defaultUUID)) {
+                sb.append(" AND id = ?");
+                params.add(filterEntity.getId());
+            }
+            if (filterEntity.getCountryId() != null && !filterEntity.getCountryId().equals(defaultUUID)) {
+                sb.append(" AND country_id = ?");
+                params.add(filterEntity.getCountryId());
+            }
+            if (filterEntity.getName() != null && !filterEntity.getName().isBlank()) {
+                sb.append(" AND name = ?");
+                params.add(filterEntity.getName());
+            }
+        }
+
+        final List<DepartmentEntity> results = new ArrayList<>();
+        try (PreparedStatement ps = getConnection().prepareStatement(sb.toString())) {
+            for (int i = 0; i < params.size(); i++) {
+                ps.setObject(i + 1, params.get(i));
+            }
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    results.add(mapResultSetToDepartmentEntity(rs));
+                }
+            }
+            return results;
+        } catch (final SQLException ex) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = "Error finding departments by filter: " + ex.getMessage();
+            throw NoseException.create(userMessage, technicalMessage, ex);
+        }
+    }
+
+    @Override
+    public DepartmentEntity findById(final UUID id) {
+        final String sql = "SELECT id, country_id, name FROM department WHERE id = ?";
+
+        try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+            ps.setObject(1, id);
+            try (ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    return mapResultSetToDepartmentEntity(rs);
+                }
+            }
+            return null;
+        } catch (final SQLException exception) {
+            var userMessage = MessagesEnum.USER_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            var technicalMessage = MessagesEnum.TECHNICAL_ERROR_SQL_EXECUTING_STATEMENT.getContent();
+            throw NoseException.create(userMessage, technicalMessage);
+        }
+    }
+
+    private DepartmentEntity mapResultSetToDepartmentEntity(final ResultSet rs) throws SQLException {
+        final DepartmentEntity entity = new DepartmentEntity();
+        try {
+            Object idObj = rs.getObject("id");
+            if (idObj instanceof UUID) {
+                entity.setId((UUID) idObj);
+            } else {
+                final String idStr = rs.getString("id");
+                entity.setId(idStr == null ? UUIDHelper.getUUIDHelper().getDefault() : UUID.fromString(idStr));
+            }
+        } catch (final Exception ex) {
+            entity.setId(UUIDHelper.getUUIDHelper().getDefault());
+        }
+
+        try {
+            Object countryObj = rs.getObject("country_id");
+            if (countryObj instanceof UUID) {
+                entity.setCountryId((UUID) countryObj);
+            } else {
+                final String cStr = rs.getString("country_id");
+                entity.setCountryId(cStr == null ? UUIDHelper.getUUIDHelper().getDefault() : UUID.fromString(cStr));
+            }
+        } catch (final Exception ex) {
+            entity.setCountryId(UUIDHelper.getUUIDHelper().getDefault());
+        }
+
+        entity.setName(rs.getString("name"));
+        return entity;
+    }
 }
+
